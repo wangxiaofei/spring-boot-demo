9d414492c396f91bdd728dcaea26dcb06f974eed
王晓飞 <xiaofei.wang@radida.com>
2018-01-03 17:35:31 +0800

    增加 | 测试类配置

--- src/test/java/com/shawn/demo/PersonTest.java
+++ src/test/java/com/shawn/demo/PersonTest.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,22 +19,27 @@ import com.shawn.demo.service.PersonService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DemoApplication.class)
 @SpringBootTest
+@TestPropertySource(properties = { "server.port=8080", "server.address=localhost" })
 public class PersonTest {
 
 	@Autowired
 	private PersonService ps;
 
 	private RestTemplate restTemplate = new RestTemplate();
+
+	@Value("${server.address}")
+	private String address;
 	
-	private int port = 8080;
+	@Value("${server.port}")
+	private int port;
 
 	@Test
 	public void personGetByIDRest() {
-		Person person = restTemplate.getForObject("http://localhost:" + port + "/person/get/1", Person.class);
+		Person person = restTemplate.getForObject("http://" + address + ":" + port + "/person/get/1", Person.class);
 		assertNotNull(person);
 		assertEquals("王五", person.getName());
 	}
-	
+
 	@Test
 	public void personGetByIDService() throws Exception {
 		Person person = ps.get(Long.valueOf(1));
