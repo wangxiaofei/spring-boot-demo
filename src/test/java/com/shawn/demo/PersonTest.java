1e9d66dd0824fbaea82ea261f02ca5b7f314d4a0
王晓飞 <xiaofei.wang@radida.com>
2018-01-03 16:53:20 +0800

    update

--- src/test/java/com/shawn/demo/PersonTest.java
+++ src/test/java/com/shawn/demo/PersonTest.java
@@ -3,20 +3,13 @@ package com.shawn.demo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.shawn.demo.domain.Person;
 import com.shawn.demo.service.PersonService;
